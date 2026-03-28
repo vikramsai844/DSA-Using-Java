285b5d6b7a565e9a7f65c5f0ff42f53dd45d80f8
vikram R <144251870+vikramsai844@users.noreply.github.com>
2026-03-28 20:06:09 +0530

    Refactor Graph to use adjacency list representation
     Graph class to use adjacency list representation instead of adjacency matrix.

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,22 +1,79 @@
 package DSA.Graph;
 
-import java.util.Random;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
+// Linked list implementation
+class LinkedList<Integer> implements Iterable<Integer> {
+    private ListNode<Integer> head;     // beginning of linked list
+    private int n;                                 // number of elements in linked list
 
+    // helper linked list class
+    private static class ListNode<Integer> {
+        private Integer data;
+        private ListNode<Integer> next;
+    }
+
+    public LinkedList() {
+        head = null;
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void add(Integer data) {
+        ListNode<Integer> oldfirst = head;
+        head = new ListNode<Integer>();
+        head.data = data;
+        head.next = oldfirst;
+        n++;
+    }
+
+    public Iterator<Integer> iterator()  {
+        return new ListIterator(head);
+    }
+
+    // an iterator, doesn't implement remove() since it's optional
+    private class ListIterator implements Iterator<Integer> {
+        private ListNode<Integer> current;
+
+        public ListIterator(ListNode<Integer> head) {
+            current = head;
+        }
+
+        public boolean hasNext()  { return current != null;                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Integer next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Integer data = current.data;
+            current = current.next;
+            return data;
+        }
+    }
+}
+// Graph representation
 public class Graph {
     private static final String NEWLINE = System.getProperty("line.separator");
+
     private final int V;
     private int E;
-    private boolean[][] adjMatrix;
-    boolean[] visited;
+    private LinkedList<Integer>[] adjList;
 
-    // empty graph with V vertices
+    // Initializes an empty graph with V vertices and 0 edges.
     public Graph(int V) {
-        if (V < 0) throw new IllegalArgumentException("Too few vertices");
+        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
         this.V = V;
         this.E = 0;
-        this.adjMatrix = new boolean[V][V];
-        visited = new boolean[V];
+        adjList = (LinkedList<Integer>[]) new LinkedList[V];
+        for (int u = 0; u < V; u++) {
+            adjList[u] = new LinkedList<Integer>();
+        }
     }
 
     // random graph with V vertices and E edges
@@ -32,51 +89,79 @@ public class Graph {
             int v = random.nextInt(V);
             addEdge(u, v);
         }
-        visited = new boolean[V];
-    }
-    // add undirected edge u-v
-    public void addEdge(int u, int v) {
-        if (!adjMatrix[u][v]) E++;
-        adjMatrix[u][v] = true;
-        adjMatrix[v][u] = true;
     }
 
-    // Refer this missing code from adjacenet matrix representation section
-
-    public void DFS(){
-        // Visit nodes using a Stack to store "to visit" nodes
-        Stack<Integer> s = new Stack<Integer>();  // Create a stack
-//      boolean[] visited= {false};  // Set all visited[i] = 0
-        s.push(0);            // Start the "to visit" at node 0
-
-        /* ===========================================
-        Loop as long as there are "active" node
-        =========================================== */
-        while( !s.isEmpty() ){
-            int nextNode;                // Next node to visit
-            int i;
-
-            nextNode = s.pop();
-
-            if ( ! visited[nextNode] ){
-                visited[nextNode] = true;    // Mark node as visited
-                System.out.println("nextNode = " + nextNode );
-
-                for ( i = 0; i < V; i++ )
-//                    for ( i = V-1; i >= 0 ; i-- )
-//                    {
-                        if ( (adjMatrix[nextNode][i] == true) && !visited[i] )
-                            s.push(i);
-//                    }
+    // Initializes a new graph.
+    public Graph(Graph G) {
+        this(G.V());
+        this.E = G.E();
+        for (int u = 0; u < G.V(); u++) {
+            // reverse so that adjacency list is in same order as original
+            Stack<Integer> reverse = new Stack<Integer>();
+            for (int v : G.adjList[u]) {
+                reverse.push(v);
+            }
+            for (int v : reverse) {
+                adjList[u].add(v);
             }
         }
     }
-    public static void main(String[] args) {    //test code
 
-        int V = 5;
-        int E = 6;
-        Graph G = new Graph(V, E);
-        System.out.println(G.toString());
-        G.DFS();
+    // Returns the number of vertices in this graph.
+    public int V() {
+        return V;
     }
+
+    // Returns the number of edges in this graph.
+    public int E() {
+        return E;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= u < V}
+    private void validateVertex(int u) {
+        if (u < 0 || u >= V)
+            throw new IllegalArgumentException("vertex " + u + " is not between 0 and " + (V-1));
+    }
+
+    public void addEdge(int u, int v) {
+        validateVertex(u);
+        validateVertex(v);
+        E++;
+        adjList[u].add(v);
+        adjList[v].add(u);
+    }
+
+    // Returns the vertices adjacent to vertex {@code u}.
+    public Iterable<Integer> adjList(int u) {
+        validateVertex(u);
+        return adjList[u];
+    }
+
+    public int degree(int u) {
+        validateVertex(u);
+        return adjList[u].size();
+    }
+
+    // Returns a string representation of this graph.
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("Undirected graph" + NEWLINE);
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int u = 0; u < V; u++) {
+            s.append(u + ": ");
+            for (int v : adjList[u]) {
+                s.append(v + " ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    // test code
+    public static void main(String[] args) {
+        Graph G = new Graph(5, 7);
+        System.out.println(G.toString());
+    }
+
+
 }
