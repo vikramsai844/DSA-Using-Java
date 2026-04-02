30b9652ae489b742746075e1649d5f23c2b3b327
vikram R <144251870+vikramsai844@users.noreply.github.com>
2026-04-02 10:57:10 +0530

    Update AllSortingMethods.java

--- Sorting/AllSortingMethods.java
+++ Sorting/AllSortingMethods.java
@@ -3,30 +3,28 @@ package DSA.Sorting;
 public class AllSortingMethods {
 
     public int[] BubbleSort(int[] A) {
-        for (int pass = A.length - 1; pass >= 0; pass--)
-        {
-            for (int i = 0; i <= pass - 1 ; i++)
-            {
-                if(A[i] > A[i+1]) {
-// swap elements
+        for (int pass = A.length - 1; pass > 0; pass--) {
+            for (int i = 0; i < pass; i++) {
+                if (A[i] > A[i + 1]) {
                     int temp = A[i];
-                    A[i] = A[i+1];
-                    A[i+1] = temp;
+                    A[i] = A[i + 1];
+                    A[i + 1] = temp;
                 }
             }
         }
         return A;
     }
 
+
     public int[] Selection(int[] A) {
         int i, j, min, temp;
         for (i = 0; i < A.length - 1; i++) {
             min = i;
-            for (j = i+1; j < A.length; j++) {
-                if(A [j] < A [min])
+            for (j = i + 1; j < A.length; j++) {
+                if (A[j] < A[min]) {
                     min = j;
+                }
             }
-            // swap elements
             temp = A[min];
             A[min] = A[i];
             A[i] = temp;
@@ -37,12 +35,12 @@ public class AllSortingMethods {
 
     public int[] InsertionSort(int[] A) {
         int i, j, v;
-        for (i = 1; i <= A.length - 1; i++) {
+        for (i = 1; i < A.length; i++) {
             v = A[i];
             j = i;
 
-            while (j >= 1 && A[j-1] > v ) {
-                A[j] = A[j-1];
+            while (j > 0 && A[j - 1] > v) {
+                A[j] = A[j - 1];
                 j--;
             }
             A[j] = v;
@@ -50,85 +48,87 @@ public class AllSortingMethods {
         return A;
     }
 
+
     public int[] ShellSort(int[] A) {
         int i, j, h, v;
-        for (h = 1; h < A.length / 3; h = 3*h+1);
 
-        for ( ; h > 0; h = h/3) {
-            for (i = h+1; i < A.length-1; i+=1) {
+        for (h = 1; h < A.length / 3; h = 3 * h + 1);
+
+        for (; h > 0; h = h / 3) {
+            for (i = h; i < A.length; i++) {
                 v = A[i];
                 j = i;
-                while (j > h && A[j-h] > v) {
-                    A[j] = A[j-h];
-                    j-=h;
+
+                while (j >= h && A[j - h] > v) {
+                    A[j] = A[j - h];
+                    j -= h;
                 }
+
                 A[j] = v;
             }
         }
         return A;
     }
 
-
-
+ 
     public void Mergesort(int[] A, int[] temp, int left, int right) {
-        int mid;
-        if(right > left) {
-            mid = (right + left) / 2;
+        if (left < right) {
+            int mid = (left + right) / 2;
+
             Mergesort(A, temp, left, mid);
-            Mergesort(A, temp, mid+1, right);
-            Merge(A, temp, left, mid+1, right);
+            Mergesort(A, temp, mid + 1, right);
+
+            Merge(A, temp, left, mid + 1, right);
         }
     }
+
+
     public void Merge(int[] A, int[] temp, int left, int mid, int right) {
-        int i, left_end, size, temp_pos;
-        left_end = mid - 1;
-        temp_pos = left;
-        size = right - left + 1;
-        while ((left <= left_end) && (mid <= right)) {
-            if(A[left] <= A[mid]) {
-                temp[temp_pos] = A[left];
-                temp_pos = temp_pos + 1;
-                left = left +1;
-            }
-            else {
-                temp[temp_pos] = A[mid];
-                temp_pos = temp_pos + 1;
-                mid = mid + 1;
+        int i = left;
+        int j = mid;
+        int k = left;
+
+        int left_end = mid - 1;
+
+        while (i <= left_end && j <= right) {
+            if (A[i] <= A[j]) {
+                temp[k++] = A[i++];
+            } else {
+                temp[k++] = A[j++];
             }
         }
-        while (left <= left_end) {
-            temp[temp_pos] = A[left];
-            left = left + 1;
-            temp_pos = temp_pos + 1;
+
+        while (i <= left_end) {
+            temp[k++] = A[i++];
         }
-        while (mid <= right) {
-            temp[temp_pos] = A[mid];
-            mid = mid + 1;
-            temp_pos = temp_pos + 1;
+
+        while (j <= right) {
+            temp[k++] = A[j++];
         }
-        for (i = 0; i < size-1; i++) {
-            A[right] = temp[right];
-            right = right-1;
+
+        // Copy back to original array
+        for (i = left; i <= right; i++) {
+            A[i] = temp[i];
         }
     }
 
 
     public static void main(String[] args) {
-        AllSortingMethods bs=new AllSortingMethods();
-        int[] A={4,5,8,2,1,6};
-//        int[] temp=bs.BubbleSort(A);
-//        int[] temp=bs.Selection(A);
+        AllSortingMethods obj = new AllSortingMethods();
 
-//        int[] temp=bs.InsertionSort(A);
+        int[] A = {4, 5, 8, 2, 1, 6};
 
-//        int[] temp=bs.ShellSort(A);
-        int[] temparr = new int[A.length];
+    
+        // int[] result = obj.BubbleSort(A);
+        // int[] result = obj.Selection(A);
+        // int[] result = obj.InsertionSort(A);
+        // int[] result = obj.ShellSort(A);
 
-       bs.Mergesort(A,temparr,0,A.length-1);
+        int[] temp = new int[A.length];
+        obj.Mergesort(A, temp, 0, A.length - 1);
 
-
-//        for(int val:temp){
-//            System.out.print(val+" ");
-//        }
+        for (int val : A) {
+            System.out.print(val + " ");
+        }
     }
 }
